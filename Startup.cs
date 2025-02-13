746990caa8d4319b3de1afb38117851e8c9ea521
Karol <87491661+karol-macas@users.noreply.github.com>
2025-02-13 08:31:38 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using PlataformaPrestamos.Data;
+using PlataformaPrestamos.Repositories;
+using PlataformaPrestamos.Security;
+using PlataformaPrestamos.Services;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>();
+        services.AddScoped<UsuarioRepository>();
+        services.AddScoped<PrestamoRepository>();
+        services.AddScoped<PagoRepository>();
+        services.AddScoped<AuthService>();
+        services.AddScoped<TokenService>();
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
